ab7cc77cd9d987e49e71c66d1e4ea3531df1fe21
Židis <95172306+MrZikis@users.noreply.github.com>
2023-09-18 18:58:39 +0200

    Update prvniProgram.java

--- src/PRG_cviceni_1/prvniProgram.java
+++ src/PRG_cviceni_1/prvniProgram.java
@@ -18,7 +18,8 @@ public class prvniProgram {
             System.out.println("Zadej jméno číslo " + (k + 1) + ":");
             jmena[k] = sc.nextLine();
         }
-
+        
+//        Tohle jsem přidal z Githubu, snad se to updatuje....     
 //        System.out.println(jmena[0]);
 //        for (int i = 0; i < jmena.length; i++) {
 //            System.out.println(jmena[i]);

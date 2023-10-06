a915c1c9de0b741cd89ab310819754a2a04c29b3
jakub.zikmund <kubikziki@seznam.cz>
2023-10-06 12:28:23 +0200

    Cvičení  - 6.10

---
+++ src/PRG_4/loops.java
@@ -0,0 +1,43 @@
+package PRG_4;
+
+import java.util.Scanner;
+
+public class loops {
+    public static void main(String[] args) {
+//        for (int i = 1; i < 101; i++) {
+//            if (i % 5 == 0) {
+//                System.out.println(i);
+//            }
+//        }
+
+        int n = 10;
+//        for (int i = 1; i < n*n+1; i++) {
+//            System.out.print(i + "\t");
+//            if (i % n == 0) {
+//                System.out.println();
+//            }
+//        }
+
+//        for (int i = 1; i < n+1; i++) {
+//            for (int j = 1; j < n+1; j++) {
+//                System.out.print((j*i) + "\t");
+//            }
+//            System.out.println();
+//        }
+
+        // WHILE
+
+//        Scanner sc = new Scanner(System.in);
+//
+//        int num = sc.nextInt();
+//
+//        while (num <= 100) {
+//            System.out.println("Zadal jsi špatnou hodnotu!");
+//            num = sc.nextInt();
+//        }
+//        System.out.println("FUCK NIGGERS");
+
+
+
+    }
+}

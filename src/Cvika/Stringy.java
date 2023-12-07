ed95ab4a4be25d64f64848f95a5b4eac832e7fb4
jakub.zikmund <kubikziki@seznam.cz>
2023-12-07 13:06:49 +0100

    stringy

---
+++ src/Cvika/Stringy.java
@@ -0,0 +1,20 @@
+package Cvika;
+
+public class Stringy {
+    String test = "testicek";
+
+    // Je tam něco ?
+    // test.contains
+
+    //Je stejný ?
+    //test.equal()
+
+    // Spojení stringů
+    // test + test
+
+    // Oddělovač
+    // text.split(znak oddělovače)
+
+    //Parsing
+    //int something = Integer.parseInt(string ktery chceme prevest);
+}

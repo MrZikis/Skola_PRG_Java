8a4ee92be49a758009b285a68c1c0504c21e692d
jakub.zikmund <kubikziki@seznam.cz>
2023-12-08 12:01:52 +0100

    stringy cviceni

--- src/Cvika/Stringy.java
+++ src/Cvika/Stringy.java
@@ -6,8 +6,11 @@ public class Stringy {
     // Je tam něco ?
     // test.contains
 
+    // Je něco někde
+    // text.charAt(index)
+
     //Je stejný ?
-    //test.equal()
+    //test.equals(string s kterým to chci porovnat)
 
     // Spojení stringů
     // test + test
@@ -17,4 +20,28 @@ public class Stringy {
 
     //Parsing
     //int something = Integer.parseInt(string ktery chceme prevest);
+
+    //Practice
+
+    //Validace RC
+    //zkontroluje, ze je RC v poho
+    //Napíše jestli je muž nebo žena
+    String rc = "000209/2121";
+//    if(rc.lenght() == 11) {
+//        System.out.println("Delka je oukej");
+//        if (rc.charAt(6) == '/') {
+//            System.out.println("Je tam správně lomítka");
+//            if (rc.charAt(2) == '5' || rc.charAt(2) == '6') {
+//                System.out.println("Je to žena");
+//            } else {
+//                System.out.println("Je to muž");
+//            }
+//        } else {
+//            System.out.println("Není tam lomítko");
+//        }
+//    } else {
+//        System.out.println("Špatná délka");
+//    }
+
+
 }
